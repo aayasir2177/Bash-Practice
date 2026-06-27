2b585d3b73f293af6c1d632b0ce2db3bedc6daa7
Yasir <aayasir217@gmail.com>
2026-06-27 18:11:28 +0500

    update script

---
+++ update_packages.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#vars
+logs_file=./logs.log
+
+#funcs
+check_exit_status(){
+if [ $? -ne 0 ]
+then
+	echo "An error occured, please check the logs file."
+fi
+}
+
+if command -v dnf &> /dev/null  
+then 
+	sudo dnf up -y &>> "$logs_file"
+	check_exit_status
+	echo "$(date)" >> "$logs_file"
+	echo "--------------------------" >> "$logs_file"
+fi
+
+
